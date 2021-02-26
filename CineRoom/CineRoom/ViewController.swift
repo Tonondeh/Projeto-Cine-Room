f38ce562d84615aeb1fc6b35c8ad17b17ff1fc33
Andre <tonondeh87@gmail.com>
2021-02-26 00:07:42 -0300

    Criacao de Projeto

---
+++ CineRoom/CineRoom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CineRoom
+//
+//  Created by ANDRE LUIZ TONON on 26/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
