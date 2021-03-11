0940cd1d55f394868cc3844ff8459b183dae47be
Alexandre Cardoso <developer.ios.alexandre@gmail.com>
2021-03-11 20:56:59 -0300

    Implementei a validação de email e senha na Login e a troca de keyboard para Decimal para a senha.

--- CineRoom/CineRoom/Features/Login/LoginViewController.swift
+++ CineRoom/CineRoom/Features/Login/LoginViewController.swift
@@ -10,87 +10,78 @@ import UIKit
 class LoginViewController: UIViewController {
 	
 	// MARK: - IBOutlet
-    @IBOutlet weak var loginImageView: UIImageView!
-    @IBOutlet weak var emailtextField: UITextField!
-    @IBOutlet weak var senhaTextField: UITextField!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configTextField()
-    }
-    
-    func configTextField() {
-        self.emailtextField.delegate = self
-        self.senhaTextField.delegate = self
-    }
-    
-    func validarEntrada() -> Bool{
-        let email = emailtextField.text ?? ""
-        let senha = senhaTextField.text ?? ""
-        var dadosOk = true
-        
-        // Validacao Email
-        
-        if email == "" {
-            dadosOk = false
-            self.emailtextField.layer.borderWidth = 1.5
-            self.emailtextField.layer.borderColor = UIColor.red.cgColor
-        }else{
-            let arrayemailCompleto = email.components(separatedBy: "@")
-            print(arrayemailCompleto)
-            if arrayemailCompleto.count != 2{
-                self.emailtextField.layer.borderWidth = 1.5
-                self.emailtextField.layer.borderColor = UIColor.red.cgColor
-                dadosOk = false
-            }else{
-                if arrayemailCompleto[1] == ""{
-                    self.emailtextField.layer.borderWidth = 1.5
-                    self.emailtextField.layer.borderColor = UIColor.red.cgColor
-                    dadosOk = false
-                }
-            }
-            
-        }
-        
-        // Validacao de senha
-        
-        if senha == "" || senha.count <= 4{
-            dadosOk = false
-            self.senhaTextField.layer.borderWidth = 1.5
-            self.senhaTextField.layer.borderColor = UIColor.red.cgColor
-            
-        }
-        return dadosOk
-    }
-    
-    @IBAction func tappedConectar(_ sender: UIButton) {
-        // Chamada de Func validar entrada
-        if validarEntrada(){
-            print("chamar tela conectar")
-        }
-        
-        
-    }
-    
-    @IBAction func tappedCriarConta(_ sender: UIButton) {
-        print("chamar tela criar conta")
-    }
-    
-    @IBAction func tappedEsqueceuSenha(_ sender: UIButton) {
-        print("chamar tela esqueceu senha")
-    }
+	@IBOutlet weak var loginImageView: UIImageView!
+	@IBOutlet weak var emailtextField: UITextField!
+	@IBOutlet weak var senhaTextField: UITextField!
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		configTextField()
+	}
+	
+	func configTextField() {
+		self.emailtextField.delegate = self
+		self.senhaTextField.delegate = self
+		
+		let toolbar = UIToolbar()
+		toolbar.barStyle = .default
+		toolbar.isTranslucent = true
+		toolbar.tintColor = .blue
+		toolbar.backgroundColor = .white
+		toolbar.sizeToFit()
+		
+		let buttonOK = UIBarButtonItem(title: "OK", style: .plain, target: self, action: #selector(finish))
+		let spaceButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+		
+		toolbar.setItems([spaceButton, buttonOK], animated: true)
+		toolbar.isUserInteractionEnabled = true
+		
+		senhaTextField.inputAccessoryView = toolbar
+		
+	}
+	
+	@objc
+	func finish() {
+		senhaTextField.resignFirstResponder()
+	}
+	
+	func validarEntrada() -> Bool{
+		
+		if emailtextField.validateEmail() && senhaTextField.validatePassword() {
+			return true
+		} else {
+			return false
+		}
+		
+	}
+	
+	
+	// MARK: - IBAction
+	@IBAction func tappedConectar(_ sender: UIButton) {
+		// Chamada de Func validar entrada
+		if validarEntrada(){
+			print("chamar tela conectar")
+			performSegue(withIdentifier: "segueHome", sender: self)
+		} else {
+			print("Entrada errada")
+		}
+	}
+	
 }
 
-extension LoginViewController:UITextFieldDelegate{
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        textField.layer.borderColor = nil
-    }
-    
+
+// MARK: - Extension TextField
+extension LoginViewController: UITextFieldDelegate {
+	
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		return true
+	}
+	
+	func textFieldDidBeginEditing(_ textField: UITextField) {
+		textField.layer.borderColor = nil
+	}
+	
 }
 
